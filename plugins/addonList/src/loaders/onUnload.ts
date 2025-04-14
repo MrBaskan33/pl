4d812370aca1459a8979266c5ad0e83915a8d974
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-14 14:07:10 +0300

    Create onUnload.ts

---
+++ plugins/addonList/src/loaders/onUnload.ts
@@ -0,0 +1,5 @@
+import { type RegisteredCommand } from ".."
+
+export function unload(registeredCommands: RegisteredCommand[]) {
+  for(const unregisterCommand of registeredCommands) unregisterCommand()
+}
