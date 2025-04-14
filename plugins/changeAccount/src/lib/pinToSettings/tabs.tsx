9b4fc619f2646b4d577e310e929bdf4a6934d68c
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-15 00:01:05 +0300

    Create tabs.tsx

---
+++ plugins/changeAccount/src/lib/pinToSettings/tabs.tsx
@@ -0,0 +1,65 @@
+import { findByProps } from '@vendetta/metro'
+import { React } from '@vendetta/metro/common'
+import { after } from '@vendetta/patcher'
+import { findInReactTree } from '@vendetta/utils'
+import type { PinToSettingsTabs } from '.'
+
+const { bunny } = window as any
+const { TableRowIcon } = findByProps('TableRowIcon')
+const tabsNavigationRef = bunny.metro.findByPropsLazy('getRootNavigationRef')
+const settingConstants = bunny.metro.findByPropsLazy('SETTING_RENDERER_CONFIG')
+const SettingsOverviewScreen = bunny.metro.findByNameLazy('SettingsOverviewScreen', false)
+
+export function patchTabsUI(tabs: PinToSettingsTabs, patches: (() => void)[]) {
+    const row = {
+        [tabs.key]: {
+            type: 'pressable',
+            title: tabs.title,
+            icon: tabs.icon,
+            IconComponent:
+                tabs.icon && (() => <TableRowIcon source={tabs.icon} />),
+            usePredicate: tabs.predicate,
+            useTrailing: tabs.trailing,
+            onPress: () => {
+                const navigation = tabsNavigationRef.getRootNavigationRef()
+                const Component = tabs.page
+
+                navigation.navigate('BUNNY_CUSTOM_PAGE', {
+                    title: tabs.title(),
+                    render: () => <Component />,
+                })
+            },
+            withArrow: true,
+        },
+    }
+
+    let rendererConfigValue = settingConstants.SETTING_RENDERER_CONFIG
+
+    Object.defineProperty(settingConstants, 'SETTING_RENDERER_CONFIG', {
+        enumerable: true,
+        configurable: true,
+        get: () => ({
+            ...rendererConfigValue,
+            ...row,
+        }),
+        set: v => (rendererConfigValue = v),
+    })
+
+    const firstRender = Symbol('pinToSettingsFlag')
+
+    patches.push(
+        after('default', SettingsOverviewScreen, (args, ret) => {
+            if (!args[0][firstRender]) {
+                args[0][firstRender] = true
+
+                const { sections } = findInReactTree(ret, i => i.props?.sections).props
+                const section = sections?.find((x: any) =>
+                    ['Bunny', 'Revenge'].includes(x.label),
+                )
+
+                if (section?.settings)
+                    section.settings = [...section.settings, tabs.key]
+            }
+        }),
+    )
+}
