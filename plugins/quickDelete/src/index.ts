98e398acdce08348404f145d1555c3505ab74b56
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-15 00:41:44 +0300

    Update index.ts

--- plugins/quickDelete/src/index.ts
+++ plugins/quickDelete/src/index.ts
@@ -1,58 +1,25 @@
-import { findByProps, findByStoreName } from "@vendetta/metro";
 import { instead } from "@vendetta/patcher";
-
-const MessageActions = findByProps("deleteMessage", "editMessage");
-const Alerts = findByProps("openLazy", "close");
-const Modals = findByProps("openModal", "closeAllModals");
-const Dispatcher = findByProps("dirtyDispatch", "dispatch");
 
 let unpatch = [];
 
 export default {
     onLoad: () => {
-        // 1. Tüm modal/popup açılışlarını engelle
+        // Tüm alert ve confirm çağrılarını engelle
         unpatch.push(
-            instead("openLazy", Alerts, () => () => {}),
-            instead("show", findByProps("show"), () => {}),
-            instead("openModal", Modals, () => () => {})
-        )
+            instead("alert", window, () => {}),
+            instead("confirm", window, () => true)
+        );
 
-        // 2. Direkt mesaj silme fonksiyonunu yakala
-        unpatch.push(instead("deleteMessage", MessageActions, (args, orig) => {
-            // Mesajı direkt sil
-            orig(args[0], args[1]);
-            
-            // Oluşabilecek tüm modalları kapat
-            Modals?.closeAllModals?.();
-            
-            // Redux dispatch'i engelle
-            if (Dispatcher) {
-                unpatch.push(
-                    instead("dispatch", Dispatcher, (action) => {
-                        if (action.type === "MODAL_OPEN") return null;
-                        return action;
-                    })
-                );
-            }
-            
-            return Promise.resolve();
-        }))
+        // React modal oluşumunu engelle
+        const React = vendetta.metro.common.React;
+        unpatch.push(
+            instead("createElement", React, (args) => {
+                if (args[0]?.name?.includes("Modal")) return null;
+                return args[0];
+            })
+        );
 
-        // 3. MessageStore'daki silme işlemini de yakala
-        const MessageStore = findByStoreName("MessageStore");
-        if (MessageStore) {
-            unpatch.push(instead("deleteMessage", MessageStore, (args, orig) => {
-                Modals?.closeAllModals?.();
-                return orig(...args);
-            }))
-        }
-
-        console.log("[Silme Bypass] Tamamen aktif!");
+        console.log("[Nükleer Bypass] Tüm popup'lar engellendi!");
     },
-
-    onUnload: () => {
-        unpatch.forEach(u => u());
-        unpatch = [];
-        console.log("[Silme Bypass] Kaldırıldı!");
-    }
-}
+    onUnload: () => unpatch.forEach(u => u())
+};
