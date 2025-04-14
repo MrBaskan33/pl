77783eb5cabb4a1151a6b7d3921bac76a5336ed1
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-15 00:33:54 +0300

    Update index.ts

--- plugins/quickDelete/src/index.ts
+++ plugins/quickDelete/src/index.ts
@@ -1,67 +1,58 @@
 import { findByProps, findByStoreName } from "@vendetta/metro";
 import { instead } from "@vendetta/patcher";
 
+const MessageActions = findByProps("deleteMessage", "editMessage");
+const Alerts = findByProps("openLazy", "close");
+const Modals = findByProps("openModal", "closeAllModals");
+const Dispatcher = findByProps("dirtyDispatch", "dispatch");
+
 let unpatch = [];
 
 export default {
     onLoad: () => {
-        // 1. Direkt API ile mesaj silme
-        const RestAPI = findByProps("deleteMessage", "editMessage");
+        // 1. Tüm modal/popup açılışlarını engelle
+        unpatch.push(
+            instead("openLazy", Alerts, () => () => {}),
+            instead("show", findByProps("show"), () => {}),
+            instead("openModal", Modals, () => () => {})
+        );
+
+        // 2. Direkt mesaj silme fonksiyonunu yakala
+        unpatch.push(instead("deleteMessage", MessageActions, (args, orig) => {
+            // Mesajı direkt sil
+            orig(args[0], args[1]);
+            
+            // Oluşabilecek tüm modalları kapat
+            Modals?.closeAllModals?.();
+            
+            // Redux dispatch'i engelle
+            if (Dispatcher) {
+                unpatch.push(
+                    instead("dispatch", Dispatcher, (action) => {
+                        if (action.type === "MODAL_OPEN") return null;
+                        return action;
+                    })
+                );
+            }
+            
+            return Promise.resolve();
+        });
+
+        // 3. MessageStore'daki silme işlemini de yakala
         const MessageStore = findByStoreName("MessageStore");
-        const ModalManager = findByProps("closeAllModals", "openModal");
-
-        if (RestAPI) {
-            // Mesaj silme fonksiyonunu override et
-            unpatch.push(instead("deleteMessage", RestAPI, (args, orig) => {
-                // Önce tüm modalları kapat
-                ModalManager?.closeAllModals?.();
-                
-                // Direkt API çağrısı yap
-                return orig(args[0], args[1]).catch(err => {
-                    console.error("Mesaj silinemedi:", err);
-                });
-            }));
-        }
-
-        // 2. MessageStore'daki silme işlemini yakala
         if (MessageStore) {
             unpatch.push(instead("deleteMessage", MessageStore, (args, orig) => {
-                ModalManager?.closeAllModals?.();
+                Modals?.closeAllModals?.();
                 return orig(...args);
             }));
         }
 
-        // 3. Tüm popup ve modal oluşumlarını engelle
-        const Dialogs = findByProps("show", "closeAll");
-        if (Dialogs) {
-            unpatch.push(instead("show", Dialogs, (args, orig) => {
-                if (args[0]?.title?.toLowerCase().includes("delete")) {
-                    ModalManager?.closeAllModals?.();
-                    return;
-                }
-                return orig(...args);
-            }));
-        }
-
-        // 4. ConfirmationModal'ı engelle
-        const ConfirmationModal = findByProps("openLazy", "closeConfirmationModal");
-        if (ConfirmationModal) {
-            unpatch.push(instead("openLazy", ConfirmationModal, (args, orig) => {
-                const [component, props] = args[0]?.() || [];
-                if (props?.title?.toLowerCase().includes("delete")) {
-                    ModalManager?.closeAllModals?.();
-                    return () => {};
-                }
-                return orig(...args);
-            }));
-        }
-
-        console.log("Silme onayı bypass aktif!");
+        console.log("[Silme Bypass] Tamamen aktif!");
     },
 
     onUnload: () => {
         unpatch.forEach(u => u());
         unpatch = [];
-        console.log("Silme onayı bypass kaldırıldı!");
+        console.log("[Silme Bypass] Kaldırıldı!");
     }
 }
