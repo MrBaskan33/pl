2b10f3c80fd7e5e4802d03488c344979a9b6640a
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-14 14:53:48 +0300

    Update details.tsx

--- plugins/staffTags/src/patches/details.tsx
+++ plugins/staffTags/src/patches/details.tsx
@@ -1,7 +1,7 @@
 import { findByProps, findByStoreName, findByTypeNameAll } from "@vendetta/metro"
 import { after } from "@vendetta/patcher"
 import { findInReactTree } from "@vendetta/utils"
-import getTag, { BUILT_IN_TAGS } from "../lib/getTag"
+import getTag, { BUILT_IN_TAGS } from "../getTag"
 
 const TagModule = findByProps("getBotLabel")
 const getBotLabel = TagModule.getBotLabel
