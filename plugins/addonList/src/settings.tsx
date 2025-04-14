0d5f5eafcfd6febab13a9624e8d2e418b0b2895f
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-14 14:04:40 +0300

    Create settings.tsx

---
+++ plugins/addonList/src/settings.tsx
@@ -0,0 +1,40 @@
+import { ReactNative } from "@vendetta/metro/common"
+import { storage } from "@vendetta/plugin"
+import { useProxy } from "@vendetta/storage"
+import { getAssetIDByName } from "@vendetta/ui/assets"
+import { ErrorBoundary, Forms } from "@vendetta/ui/components"
+const { FormSwitchRow, FormIcon, FormSection } = Forms
+const Icons = {
+  List: getAssetIDByName("ic_list")
+}
+
+export default () => {
+  useProxy(storage)
+
+  return (
+    <ErrorBoundary>
+      <ReactNative.ScrollView style = {{ flex: 1 }}>
+
+        <FormSection title = "Plugin List" titleStyleType = "no_border" >
+          <FormSwitchRow
+            label = "Always send detailed list"
+            subLabel = "This will keep the 'detailed' option always set to 'True'"
+            leading = { <FormIcon source = {Icons.List} /> }
+            onValueChange = { (value: boolean) => storage.pluginListAlwaysDetailed = value }
+            value = { storage.pluginListAlwaysDetailed }
+          />
+        </FormSection>
+        <FormSection title = "Theme List" titleStyleType = "no_border" >
+          <FormSwitchRow
+            label = "Always send detailed list"
+            subLabel = "This will keep the 'detailed' option always set to 'True'"
+            leading = { <FormIcon source = {Icons.List} /> }
+            onValueChange = { (value: boolean) => storage.themeListAlwaysDetailed = value }
+            value = { storage.themeListAlwaysDetailed }
+          />
+        </FormSection>
+
+      </ReactNative.ScrollView>
+    </ErrorBoundary>
+  ) 
+}
