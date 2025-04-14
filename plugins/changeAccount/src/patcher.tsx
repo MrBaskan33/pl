d7a5df31705b2293860831f4a341c63e3584ce11
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-14 23:58:47 +0300

    Create patcher.tsx

---
+++ plugins/changeAccount/src/patcher.tsx
@@ -0,0 +1,22 @@
+import { React } from '@vendetta/metro/common'
+import { getAssetIDByName } from '@vendetta/ui/assets'
+import { patchSettingsPin } from '$/lib/pinToSettings'
+import CustomSettingsPage from '../components/CustomSettingsPage'
+
+export default (): (() => void) => {
+    const patches: (() => void)[] = []
+
+    patches.push(
+        patchSettingsPin({
+            key: 'custom-settings-page',
+            icon: getAssetIDByName('ic_settings_24px'), // İsteğe göre değiştirilebilir
+            title: () => 'Özel Ayar',
+            predicate: () => true,
+            page: CustomSettingsPage,
+        }),
+    )
+
+    return () => {
+        for (const x of patches) x()
+    }
+}
