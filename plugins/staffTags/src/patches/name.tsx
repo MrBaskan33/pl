d6ed30eadafe64a2ea32e8339ad48329d9a48cf3
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-14 14:49:17 +0300

    Update name.tsx

--- plugins/staffTags/src/patches/name.tsx
+++ plugins/staffTags/src/patches/name.tsx
@@ -1,53 +1,50 @@
-import { findByName, findByProps, findByStoreName } from "@vendetta/metro";
-import { after } from "@vendetta/patcher";
-import { findInReactTree } from "@vendetta/utils";
-import getTag, { BUILT_IN_TAGS } from "../lib/getTag";
+import { findByName, findByProps, findByStoreName } from "@vendetta/metro"
+import { after } from "@vendetta/patcher"
+import { findInReactTree } from "@vendetta/utils"
+import getTag, { BUILT_IN_TAGS } from "../lib/getTag"
 
-const DisplayName = findByName("DisplayName", false);
-const HeaderName = findByName("HeaderName", false);
-
-const TagModule = findByProps("getBotLabel");
-const getBotLabel = TagModule.getBotLabel;
-
-const GuildStore = findByStoreName("GuildStore");
-const ChannelStore = findByStoreName("ChannelStore");
+const DisplayName = findByName("DisplayName", false)
+const HeaderName = findByName("HeaderName", false)
+const TagModule = findByProps("getBotLabel")
+const getBotLabel = TagModule.getBotLabel
+const GuildStore = findByStoreName("GuildStore")
+const ChannelStore = findByStoreName("ChannelStore")
 
 export default () => {
-    const patches = []
+  const patches = []
 
-    patches.push(after("default", HeaderName, ([{ channelId }], ret) => {
-        ret.props.channelId = channelId
-    }))
+  patches.push(after("default", HeaderName, ([{ channelId }], ret) => {
+    ret.props.channelId = channelId
+  }))
 
-    patches.push(after("default", DisplayName, ([{ guildId, channelId, user }], ret) => {
-        const tagComponent = findInReactTree(ret, (c) => c.type.Types)
-        if (!tagComponent || !BUILT_IN_TAGS.includes(getBotLabel(tagComponent.props.type))) {
-            const guild = GuildStore.getGuild(guildId)
-            const channel = ChannelStore.getChannel(channelId)
-            const tag = getTag(guild, channel, user)
+  patches.push(after("default", DisplayName, ([{ guildId, channelId, user }], ret) => {
+    const tagComponent = findInReactTree(ret, (c) => c.type.Types)
+    if(!tagComponent || !BUILT_IN_TAGS.includes(getBotLabel(tagComponent.props.type))) {
+      const guild = GuildStore.getGuild(guildId)
+      const channel = ChannelStore.getChannel(channelId)
+      const tag = getTag(guild, channel, user)
 
-            if (tag) {
-                if (tagComponent) {
-                    tagComponent.props = {
-                        type: 0,
-                        ...tag
-                    }
-                } else {
-                    const row = findInReactTree(ret, (c) => c.props.style.flexDirection === "row")
-                    row.props.children.push(
-                        <TagModule.default
-                            style={{ marginLeft: 0 }}
-                            type={0}
-                            text={tag.text}
-                            textColor={tag.textColor}
-                            backgroundColor={tag.backgroundColor}
-                            verified={tag.verified}
-                        />
-                    )
-                }
-            }
+      if(tag) {
+        if(tagComponent) {
+          tagComponent.props = {
+            type: 0,
+            ...tag
+          }
+        } else {
+          const row = findInReactTree(ret, (c) => c.props.style.flexDirection === "row")
+          row.props.children.push(
+          <TagModule.default
+            style = {{ marginLeft: 0 }}
+            type = { 0 }
+            text = { tag.text }
+            textColor = { tag.textColor }
+            backgroundColor = { tag.backgroundColor }
+            verified = { tag.verified }
+          />
+          )
         }
-    }))
-
-    return () => patches.forEach((unpatch) => unpatch())
+      }
+    }
+  }))
+  return () => patches.forEach((unpatch) => unpatch())
 }
