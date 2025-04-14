ebf6926c0fb359002576264d2cb2e8cdf48b01a3
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-14 20:08:20 +0300

    Create index.ts

---
+++ plugins/alwaysTrust/src/index.ts
@@ -0,0 +1,19 @@
+import { after } from "@vendetta/patcher"
+import { findByStoreName } from "@vendetta/metro"
+
+const MaskedLink = findByStoreName("MaskedLinkStore")
+let patches = []
+
+export default {
+  onLoad: () => {
+    patches.push(
+      after("isTrustedDomain", MaskedLink, () => {
+        return true
+      }),
+    )
+  },
+    
+  onUnload: () => {
+    for(const unpatch of patches) unpatch()
+  },
+}
