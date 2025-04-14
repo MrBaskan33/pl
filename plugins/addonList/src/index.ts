69e1048b1c39abfb07d5c0e72826f6708edcae4d
MrBaskan <117309746+MrBaskan33@users.noreply.github.com>
2025-04-14 13:57:04 +0300

    Create index.ts

---
+++ plugins/addonList/src/index.ts
@@ -0,0 +1,13 @@
+import { load } from "./loaders/onLoad"
+import { unload } from "./loaders/onUnload"
+import settings from "./settings"
+
+export type RegisteredCommand = (() => void)
+
+let registeredCommands: RegisteredCommand[]
+
+export default {
+  onLoad: () => { registeredCommands = load() },
+  onUnload: () => { unload(registeredCommands) },
+  settings
+}
